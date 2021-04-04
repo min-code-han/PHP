ed4c46c79f9c898bd62858e2d09c70a48cf4644b
mina <yegrinaaa@gmail.com>
2021-04-05 00:46:44 +0900

    파라미터 변수할당

---
+++ parameter.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+Hello,<?php echo $_GET['address']; ?>에 사시는  <?php echo $_GET['name']; ?>님
+<!-- http://127.0.0.1:8080/parameter.php?name=mina&address=Seoul -->
+</body>
+</html>
