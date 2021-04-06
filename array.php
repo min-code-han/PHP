10ec6c2cd79810c80f3d82c2853e71ecdcdb47ef
mina <yegrinaaa@gmail.com>
2021-04-06 22:28:00 +0900

    array_push

---
+++ array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Array</title>
+</head>
+<body>
+  <h1>Array</h1>
+  <?php
+    $coworkers = array('mina', 'hoon', 'lee', 'baba');
+    array_push($coworkers, 'han','kyung');
+    var_dump($coworkers);
+
+  ?>
+</body>
+</html>
